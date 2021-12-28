7babdf83b175a7b18008736f8e106edcb484fbed
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-12-28 12:17:06 -0600

    Create NumberTools.java

---
+++ tools/NumberTools.java
@@ -0,0 +1,44 @@
+/*  Original Licensing Copyright
+ * 
+ *  Utility class containing common methods to process numbers.
+ *  Copyright (C) 2021  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.dz_fs_dev.math.tools;
+
+/**
+ * Utility class containing common methods to process numbers.
+ * 
+ * @author DZ_FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+public final class NumberTools {
+	private NumberTools() {}
+	
+	/**
+	 * Performs a primality test; returns true if the specified number is prime and false otherwise.
+	 * 
+	 * @param number The specified number to undergo the primality test.
+	 * @return True if the specified number is prime and false otherwise.
+	 */
+	public static boolean isPrime(long number) {
+		for(long i = 2; i < number; i = i % 2 == 1 ? 2 : 1) {
+			if(number % i == 0) return false;
+		}
+		
+		return true;
+	}
+}
