05fad79f3c48986974111d3aefc44196fbf85e08
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-27 10:25:23 -0600

    Update ctor1 init tests

--- matrices/tests/MatrixTests.java
+++ matrices/tests/MatrixTests.java
@@ -20,36 +20,60 @@ import org.junit.jupiter.params.provider.CsvSource;
  */
 class MatrixTests {
 	@ParameterizedTest
-	@CsvSource(
-			"10,15,8.505,java.lang.Double"
-			)
-	void constructor1_initializesCorrectSizeMatrix(
-			int rows, int columns, String fillString, String type) 
+	@CsvSource({
+		"10,15,8.505,java.lang.Double",
+		"10,15,8,java.lang.Integer",
+		"10,15,9,java.lang.Long"
+	})
+	void constructor1_initializesCorrectly(
+			int rows, int columns, String fillString, String typeStr) 
 					throws InstantiationException, IllegalAccessException,
 					IllegalArgumentException, InvocationTargetException,
 					NoSuchMethodException, SecurityException,
 					ClassNotFoundException, NoSuchFieldException {
 		// Arrange
-		Class<?> clazz = Class.forName(type);
-		Number fill = (Number) clazz.getConstructor(String.class)
+		Class<?> type = Class.forName(typeStr);
+		Number fill = (Number) type.getConstructor(String.class)
 				.newInstance(fillString);
 		Field dataField = Matrix.class.getDeclaredField("data");
 		dataField.setAccessible(true);
 
 		// Act
-		Matrix<Double> target = new Matrix<Double>(rows, columns, fill);
+		Matrix<?> target = new Matrix<>(rows, columns, fill);
 
 		// Access
-		Object[][] targetDataField = (Object[][]) dataField.get(target);
+		@SuppressWarnings("unchecked")
+		T[][] targetDataField = (T[][]) dataField.get(target);
 
 		// Assert
 		assertAll(
-				() -> assertEquals(columns, targetDataField.length),
+				() -> assertEquals(columns, targetDataField.length, "The backing data does not match the specified row count."),
+				
 				() -> Arrays.spliterator(targetDataField)
 				.forEachRemaining(new Consumer<Object[]>() {
 					@Override
 					public void accept(Object[] t) {
-						fail();
+						assertEquals(rows, t.length, "The backing data does not match the specified row count.");
+					}
+				}),
+				
+				() -> assertInstanceOf(type.arrayType().arrayType(), targetDataField, "The backing data is typed incorrectly."),
+				
+				() -> Arrays.spliterator(targetDataField)
+				.forEachRemaining(new Consumer<Object[]>() {
+					@Override
+					public void accept(Object[] t) {
+						assertEquals(rows, t.length, "The backing data does not match the specified row count.");
+					}
+				}),
+				
+				() -> Arrays.spliterator(targetDataField)
+				.forEachRemaining(new Consumer<Object[]>() {
+					@Override
+					public void accept(Object[] t) {
+						Object[] arrExpected = (Object[])Array.newInstance(type, rows);
+						Arrays.fill(arrExpected, fill);
+						assertArrayEquals(arrExpected, t, "The backing data was not filled with the specified fill value.");
 					}
 				})
 				);
